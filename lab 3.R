fe5b26c363ce254650cdbe2914038a854e5f4e80
Zoe Zhou <41186485+zoeyouyu@users.noreply.github.com>
2020-04-29 17:18:31 +1200

    lab1 and 3 and assignment 1 files.

--- lab 3.R
+++ lab 3.R
@@ -1,100 +1,122 @@
-library(scatterplot3d)
-#read.csv(file.choose()) -> LCMS
-LCMS = read.csv(list.files(pattern = "LCMS"))
 x = rnorm(100)
-y = rnorm(100)
-z = x + y + rnorm(100, sd = 0.1)
+y = x * 3 + rnorm(100)
+
+# scale the data
 x = (x - mean(x)) / sd(x)
 y = (y - mean(y)) / sd(y)
-z = (z - mean(z)) / sd(z)
-dat = cbind(x, y, z)
+dat = cbind(x, y)
+smallex = prcomp(dat)
 
-# PCA, scale or not?
-smallex = prcomp(dat, scale = TRUE)
+par(mfrow = c(1,2), pty = 's')
+plot(x, y, main = 'Original Data', ylim = c(-4, 4), xlim = c(-4, 4))
+plot(smallex$x[ ,1:2], main = 'First 2 PC scores', ylim = c(-4, 4), xlim = c(-4, 4))
 
-# What does it contain?
-names(smallex)
-
-# Scale, if 1, already scaled
-smallex$scale
-
-# Eigenvectors, coefficients used to create the PCs in rotation
-smallex$rotation
-
-# Scores 
-smallex$x
-
-# sd for 
-smallex$sdev
-apply(smallex$x, 2, sd)
-
-# Screeplot, showing sd^2 = variance
-screeplot(smallex)
-
-
-## Showing 3D
-scat = scatterplot3d(dat)
-
-# Reconstruction using first 2 PCs
-scat$points3d(t((smallex$rotation[, 1:2]) %*% t(smallex$x[,1:2])), col = 2)
-
-# *10 just to make it longer, showing the direction of PC1
-scat$points3d(rbind(c(0, 0, 0), smallex$rotation[, 1] * 10), type = 'l', col = 3)
-
-# Showing direction for PC2
-scat$points3d(rbind(c(0, 0, 0), smallex$rotation[, 2] * 10), type = 'l', col = 3)
-
-
-LCMS = read.csv('LCMS.reduced.csv')
-names(LCMS)
-
-# Divide data into 2 subsets
-samp.info = LCMS[, 1:2]
-LCMS = LCMS[, -c(1:2)]
-
-# We have 10 batches
-table(samp.info$Batch)
-
-# Check data
-hist(LCMS[, 1])
-# Comment, highly skewed, so we need to log the data
-LCMS = log(LCMS)
-hist(LCMS[, 1])
-
-# Do we have missing data?
-image(is.na(LCMS))
-
-# Data cleaning
-# Count how many missing in each column
-missing = apply(is.na(LCMS), 2, sum)
-hist(missing)
-
-LCMS = LCMS[, missing == 0]
-sds = apply(LCMS, 2, sd)
-LCMS = LCMS[, sds > 0.001]
-
-
-# PCA
-LCcomp = prcomp(LCMS, center = T, scale = T)
-screeplot(LCcomp)
-
-# Or
-plot(LCcomp$sd^2, xlab = 'component', 
-     ylab = 'variance', main = 'LCMS screeplot')
-abline(h = 1)
-
-# how much variance first 3 PCs represented?
-LCcomp$sd[1:3]^2/sum(LCcomp$sd^2)
-
-scatterplot3d(LCcomp$x[, 1:3])
-pairs(LCcomp$x[, 1:5])
-
-# Is batch important?
-scatterplot3d(LCcomp$x[, 1:3], color = samp.info$Batch)
-# comment, we can see clustering by color, so yes, batch is important
+# Our x, y axis are the same scale
+sum(smallex$rotation[, 1]^2)
+sum(smallex$rotation[, 2]^2)
+# Our constraint: sum of the square of coefficient = 1
 
 
 
+# Now, We will make the sd of the scores = 1 
+# Yhe rotation will have different length
+# The important axis will have longer length
+# still score * axis
+
+# STANDARLISED THE SCORES
+# So each has sd = 1
+scaleex = list(rotation = smallex$rotation, 
+               x = smallex$x, 
+               sd = smallex$sd)
+
+# Divide each column by sd
+for (i in 1:2) {
+  scaleex$x[ ,i] = scaleex$x[,i] / scaleex$sd[i];
+  scaleex$rotation[ ,i] = scaleex$rotation[ ,i] * scaleex$sd[i]
+}
+
+# score * rotation vector still has the same value
+# the original data values if we use all components
+
+plot(scaleex$x[,1:2], xlim = c(-4, 4), ylim = c(-4, 4),
+     main = 'Scaled PC scores')
+
+# It contains the correlation 
+scaleex$rotation
+# check
+cor(x, scaleex$x[, 1])
+
+# Easy to interpret with correlation
+
+# Rotating
+varimax(smallex$rotation)
+
+### Real example
+loans = read.csv("loans-subset.csv")
+loans.pr = prcomp(loans[, 2:11], scale = TRUE)
+
+plot(loans.pr,
+     main = "PCA scree plot for scaled loan data",
+     xlab = "Component")
+
+round(loans.pr$sd^2 / sum(loans.pr$sd^2), 2)
+loans.pr$sd
+pairs(loans.pr$x[, 1:3], col = loans$Loans.loan_status,
+      main = "First three PC", pch = ".")
+
+# Two groups overlapping each other, nothing differentiate them
+
+loans.prscaled = list(sd = loans.pr$sd[1:3], 
+                      scores = loans.pr$x[ ,1:3], 
+                      loadings = loans.pr$rotation[ ,1:3])
+
+for (i in 1:3){
+  loans.prscaled$loadings[ ,i] = loans.prscaled$loadings[ ,i] * loans.prscaled$sd[i]
+  loans.prscaled$x[ ,i] = loans.prscaled$x[ ,i] / loans.prscaled$sd[i]}
+
+# Make it a loading matrix so we can cut off
+
+class(loans.prscaled$loadings) = "loadings"
+
+# Dont show me anything less than 0.2 (blank)
+print(loans.prscaled$loadings, cutoff = .2)
+
+# Challenging interpretation
+
+# Now rotate
+varrot = varimax(loans.prscaled$loadings)
+print(varrot$loadings, cutoff = .2)
+
+# Fewer variables are left
+# What does PC1 means? The size of loan
+# Fewer variables are associated with each PC
+# The variables with big coefficients are highly correlated
 
 
+# PC2 - how much debt do you have
 
+# PC3 - how long you been around, track record is?
+
+# Rotate allows us to intepret it
+
+# FACTOR ANALYSIS
+loan.fac = factanal(loans[, 2:11], 3, scores = "regression")
+loan.fac = factanal(loans[, 2:11], 3, scores = "regression", nstart = 10)
+
+
+loan.fac$converged
+loan.fac
+
+# Uniqueness is not ok
+
+# Drop one
+loan.fac2 = factanal(loans[ ,3:11], 2, scores = "regression")
+loan.fac2
+
+# p value, not good, we dont have adequent factors
+
+# Check factor scores
+plot(loan.fac2$scores, pch = ".", col = loans[ ,12])
+
+
+# We have some banding going on
